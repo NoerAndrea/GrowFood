6235fae6b32866ed7b5e379df48c73fa01e12240
Andrea Noer <andreanoer@gmail.com>
2024-04-29 21:37:45 -0300

    setup deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],  
+  //passo 1 - npm run build
+  //passo 2 -npm run preview
+  //apontar para a porta 80
+  preview: {
+    host: true, //habilita 
+    port: 80
+  }
+  //passo 3 - arquivo vercel
 })
